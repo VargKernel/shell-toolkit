61bb01deed3ac8ccb39b77b01d5bf4b518409d96
rebootless <sbox.simple@protonmail.com>
2026-07-31 21:39:48 +0300

    Update dependencies comment in install-i3-wm.sh

--- standalone/install/apt/cli/install-i3-wm.sh
+++ standalone/install/apt/cli/install-i3-wm.sh
@@ -6,7 +6,7 @@
 # sudo: true
 # interactive: false
 # idempotent: true
-# dependencies: 01-install-xorg.sh
+# dependencies: none
 # ---DOC-END---
 set -euo pipefail
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
